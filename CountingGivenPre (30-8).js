1569f8c6776edf99c373bde44721c1f11510b216
Mohammed Ashiq A <140760724+ashiqmnb@users.noreply.github.com>
2024-08-30 09:34:56 +0530

    Add files via upload

---
+++ CountingGivenPre (30-8).js
@@ -0,0 +1,33 @@
+// 2185. Counting Words With a Given Prefix
+// You are given an array of strings words and a string pref.
+// Return the number of strings in words that contain pref as a prefix.
+// A prefix of a string s is any leading contiguous substring of s.
+
+// Example 1:
+
+// Input: words = ["pay","attention","practice","attend"], pref = "at"
+// Output: 2
+// Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+// Example 2:
+
+// Input: words = ["leetcode","win","loops","success"], pref = "code"
+// Output: 0
+// Explanation: There are no strings that contain "code" as a prefix.
+
+const prefixCount = function(words, pref) {
+    let count = 0;
+    words.forEach((word,index)=>{
+        if(word.startsWith(pref)){
+            count++
+        }
+    })
+    return count
+};
+
+let words = ["pay","attention","practice","attend"]
+let pref = "at"
+
+// let words = ["leetcode","win","loops","success"]
+// let pref = "code"
+
+console.log(prefixCount(words,pref));
