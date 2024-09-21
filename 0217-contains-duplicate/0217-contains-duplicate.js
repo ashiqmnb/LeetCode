9a13e5b1458824087c9ba7af6663b6583dc883e5
Mohammed Ashiq A <140760724+ashiqmnb@users.noreply.github.com>
2024-09-21 09:22:29 +0530

    Time: 141 ms (9.76%), Space: 59.9 MB (87.76%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort((a,b)=> a-b)
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] === nums[i-1]){
+            return true
+        }
+    }
+
+    return false
+
+    // for(num of nums){
+    //     if(nums.indexOf(num) === nums.lastIndexOf(num)){
+    //         return false
+    //     }
+    //     else{
+    //         return true
+    //     }
+    // }
+};
