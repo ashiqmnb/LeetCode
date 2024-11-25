2bb3233599e7fe2dde5b09708045298348c7306d
Mohammed Ashiq A <140760724+ashiqmnb@users.noreply.github.com>
2024-11-25 10:10:52 +0530

    Time: 634 ms (92.64%), Space: 0B (100%) - LeetHub

---
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+select 
+    u.name as name,
+    isnull(sum(r.distance), 0) as travelled_distance
+
+from Users u
+left join Rides r
+on u.id = r.user_id
+group by u.id, u.name
+order by 
+    travelled_distance desc,
+    name asc;
