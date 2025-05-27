0dd315fe2e4b372152556bc9da6fb088314010e1
Mohammed Ashiq A <140760724+ashiqmnb@users.noreply.github.com>
2025-05-27 10:08:13 +0530

    Time: 0 ms (100%), Space: 48.1 MB (22.47%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cs
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cs
@@ -8,21 +8,5 @@ public class Solution {
         }
 
         return letters[0];
-
-        // int left = 0;
-        // int right = letters.Length - 1;
-
-        // while (left <= right) {
-        //     int mid = left + (right - left) / 2;
-
-        //     if (letters[mid] <= target) {
-        //         left = mid + 1;
-        //     } else {
-        //         right = mid - 1;
-        //     }
-        // }
-
-        // If left goes out of bounds, wrap around to the first character
-        // return letters[left % letters.Length];
     }
 }
