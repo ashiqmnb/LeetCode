e5016117bd9ec18da761edd111093aab65aa83d1
Mohammed Ashiq A <140760724+ashiqmnb@users.noreply.github.com>
2025-06-06 13:31:38 +0530

    Time: 6 ms (13.24%), Space: 42.4 MB (5.88%) - LeetHub

--- 1137-height-checker/1137-height-checker.cs
+++ 1137-height-checker/1137-height-checker.cs
@@ -1,11 +1,12 @@
 public class Solution {
     public int HeightChecker(int[] heights) {
-        var heightSorted =(int[])heights.Clone();
-        Array.Sort(heightSorted);
-        var count = 0;
+        int[] expected = heights.OrderBy(n => n).ToArray();
+
+        int count = 0;
+
         for(int i = 0; i < heights.Length; i++){
-            if(heights[i] != heightSorted[i]){
-                ++count;
+            if(heights[i] != expected[i]){
+                count++;
             }
         }
         return count;
